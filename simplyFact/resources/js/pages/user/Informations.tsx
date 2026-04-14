f15c4ecc0283d9125bede6c00c02f0fda311bef9
Lisa Sauvinet <sauvinetlisa@gmail.com>
2026-04-14 12:21:09 +0200

    feat: page Informations,  modify variables (#4)

--- simplyFact/resources/js/pages/user/Informations.tsx
+++ simplyFact/resources/js/pages/user/Informations.tsx
@@ -1,29 +1,23 @@
 import { useForm, router, Head } from '@inertiajs/react';
 import Header from "@/layouts/Header"
 import { Button, TextField } from "@mui/material"
-import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import dayjs from 'dayjs'
 
 interface InformationsProps {
     // Définir les propriétés nécessaires pour le composant Informations
     expensesClaim?: { id: number };
     informations?: {
-        commission: string;
-        object: string;
-        dateStart: string;
-        dateEnd: string;
+        commitee_name: string;
+        action_name: string;
+        action_dates: string;
     };
 }
 
 export default function Informations({ expensesClaim, informations }: InformationsProps) {
 
     const { data, setData, post, errors, reset } = useForm({
-        commission: informations ? informations.commission : "",
-        object: informations ? informations.object : "",
-        dateStart: informations ? informations.dateStart : "",
-        dateEnd: informations ? informations.dateEnd : "",
+        commitee_name: informations ? informations.commitee_name : "",
+        action_name: informations ? informations.action_name : "",
+        action_dates: informations ? informations.action_dates : "",
     });
 
     function completeStep() {
@@ -57,61 +51,48 @@ export default function Informations({ expensesClaim, informations }: Informatio
                         <TextField
                             label="Commission"
                             slotProps={{ inputLabel: { shrink: true } }}
-                            defaultValue={data.commission !== "" ? data.commission : ''}
-                            onChange={(e) => setData('commission', e.target.value)}
+                            defaultValue={data.commitee_name !== "" ? data.commitee_name : ''}
+                            onChange={(e) => setData('commitee_name', e.target.value)}
                             fullWidth
                             size= "small"
                         />
-                        {errors.commission && <span>{errors.commission}</span>}
+                        {errors.commitee_name && <span>{errors.commitee_name}</span>}
 
                         <TextField
                             label="Objet de l'action"
                             slotProps={{ inputLabel: { shrink: true } }}
-                            defaultValue={data.object !== "" ? data.object : ''}
-                            onChange={(e) => setData('object', e.target.value)}
+                            defaultValue={data.action_name !== "" ? data.action_name : ''}
+                            onChange={(e) => setData('action_name', e.target.value)}
                             fullWidth
                             size= "small"
                         />
-                        {errors.object && <span>{errors.object}</span>}
+                        {errors.action_name && <span>{errors.action_name}</span>}
 
-                        <div className="flex flex-row gap-2 mb-3">
-
-                            <LocalizationProvider dateAdapter={AdapterDayjs}>
-
-                                <DatePicker
-                                    label="Date de début"
-                                    value={data.dateStart ? dayjs(data.dateStart) : null}
-                                    onChange={(newValue) => setData('dateStart', newValue ? newValue.format('YYYY-MM-DD') : '')}
-                                    slotProps={{ textField: { fullWidth: true, size: 'small' } }}
-                                />
-                                {errors.dateStart && <span>{errors.dateStart}</span>}
-
-                                <DatePicker
-                                    label="Date de fin"
-                                    value={data.dateEnd ? dayjs(data.dateEnd) : null}
-                                    onChange={(newValue) => setData('dateEnd', newValue ? newValue.format('YYYY-MM-DD') : '')}
-                                    slotProps={{ textField: { fullWidth: true, size: 'small' } }}
-                                />
-                                {errors.dateEnd && <span>{errors.dateEnd}</span>}
-
-                            </LocalizationProvider>
-
-                        </div>
-
-                        <Button
-                            type="submit"
-                            variant="contained"
+                        <TextField
+                            label="Date de début"
+                            slotProps={{ inputLabel: { shrink: true } }}
+                            defaultValue={data.action_dates !== ""? data.action_dates : ''}
+                            onChange={(e) => setData('action_dates', e.target.value)}
                             fullWidth
-                            className="!mt-5"
-                            sx={{
-                                backgroundColor: '#2D6A2D',
-                                '&:hover': { backgroundColor: '#1F4F1F' },
-                            }}
-                        >
-                            Suivant
-                        </Button>
-                        
+                            size= "small"
+                        />
+                        {errors.action_dates && <span>{errors.action_dates}</span>}
+
+
                     </div>
+
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        fullWidth
+                        className="!mt-5"
+                        sx={{
+                            backgroundColor: '#2D6A2D',
+                            '&:hover': { backgroundColor: '#1F4F1F' },
+                        }}
+                    >
+                        Suivant
+                    </Button>
                 
                 </form>
             
